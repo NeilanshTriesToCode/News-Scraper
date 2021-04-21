813c9a47e4586510d5375d7347176493a757e901
NeilanshTriesToCode <neilanshjr@gmail.com>
2021-04-21 23:33:16 +0530

    coded scraper to search for news query

--- index.js
+++ index.js
@@ -3,29 +3,30 @@ const request = require('request-promise');  // scraper for static websites
 const $ = require('cheerio');
 const pupeeteer = require('puppeteer');      // scraper for dynamic websites
 
-var url = 'https://news.google.com/search?q=fc%20barcelona&hl=en-IN&gl=IN&ceid=IN%3Aen';
+var url = 'https://news.google.com/topstories?hl=en-IN&gl=IN&ceid=IN:en';   // search for news at google news
+
 pupeeteer
-    .launch()
+    .launch({headless: false})
     .then((browser) => { return browser.newPage() })
     .then((page) => { 
-        return page.goto(url).then(() =>{
-            return page.content();         // returns html of the page
+        return page.goto(url).then(async () =>{
+            await page.setViewport({ width: 1920, height: 1080 });   // open browser window to show puppeteer's actions
+            await page.focus('.yNVtPc.ZAGvjd.Ny5lGc');     // focus on the search bar
+            await page.click('.yNVtPc.ZAGvjd.Ny5lGc');     // click on the search bar
+            await page.keyboard.type('fc barcelona');      // search for the given term
+            await page.keyboard.press('Enter');           
+            // page.screenshot({path: 'sc.png'});            // DEBUG
+            await page.waitForNavigation();
+            return page.content();         // returns html content of the page
         });
     })
-    .then((html) => {
-       //$('.gLFyf gsfi').text = 'fc barcelona';
-       var headlines = $('.DY5T1d.RZIKme', html).text();
-      /* for(var i = 0; i < headlines.length; i++){
-        console.log(headlines[i].text());
-        console.log('\n');
-       } */
-       console.log(headlines);
-      
-       console.log('success');
-    })
+    .then(async (html) => {
+        // work with html content to retrieve news headlines
+    }) 
     .catch((err) => {
         console.log(err);
     });
+    
     
 
 
