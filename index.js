0a255f9f78fcbf84c824e6b5f6d6c4663faff9ab
NeilanshTriesToCode <neilanshjr@gmail.com>
2021-04-29 16:44:00 +0530

    finally able to retrieve links and headlines

--- index.js
+++ index.js
@@ -13,9 +13,22 @@ pupeeteer
             await page.setViewport({ width: 1920, height: 1080 });   // open browser window to show puppeteer's actions
             // await page.focus('.gLFyf.gsfi');     // focus on the search bar
             await page.click('.gLFyf.gsfi');     // click on the search bar
-            await page.keyboard.type('basketball news');      // search for the given term
+            await page.keyboard.type('football news');      // search for the given term
             await page.keyboard.press('Enter');   
-            await page.waitFor(3000);   
+            await page.waitFor(3000);  
+           /* var html = await page.content(); 
+            const $ = cheerio.load(html);
+            var nav_links = $('.MUFPAc > .hdtb-mitem').children();  // get top nav link elements after Google search
+            
+           
+             - nav_link returns an object list of objects. 
+             - Each object inside the object-list is an HTML link node/element.
+             - These links point to different pages of the search and 
+               are in this order as seen on the Google page: [All  Shopping  Images  News  Videos  More]
+             - We'll select on the link node/element from the object-list which directs to the News page, 
+               which is at index 4 of the object list.
+            */
+            //console.log(nav_links[4]['children'][1]['data']);
             await page.click('.Q2MMlc');     
             // page.screenshot({path: 'sc.png'});            // DEBUG
             await page.waitFor(3000);   
@@ -25,12 +38,12 @@ pupeeteer
     .then(async (html) => {
         // work with html content to retrieve news headlines
         const $ = cheerio.load(html);
-        var for_links = $('.nChh6e.DyOREb > div > .dbsr').children();  // object containing element for link and its children
+        var for_links = $('div > .nChh6e.DyOREb > div > div > .dbsr').children();  // object containing element for link and its children
         //console.log(for_links.length);
         return for_links;  // returns array containing two objects containing html info about news cards (healdines and links)
-    })
+    }) 
     .then(async (news_links) => {
-        /* BASIC STRUCTURE OF A NEWS-CARD DOM:
+       /*  BASIC STRUCTURE OF A NEWS-CARD DOM:
          - Each news item is inside an HTML element called 'g-card'
          - Every content (news headline, news image, sub headline, date, etc.) is inside a link element, which is a child of 'g-card'
          - This function receives an object which contains the list of all the link elements inside all 'g-card's
@@ -41,7 +54,7 @@ pupeeteer
          - This function receives an "object of objects" containing a list of all the link elements/nodes. 
          - Every node/element is represented as an object, with its attributes, children, parent node, children nodes 
            specified as key-value pairs inside the node/element object.
-         - The 'children' key inside a node/element object has an array of that node/element children in the DOM tree.
+         - The 'children' key inside a node/element object has an array of that node/element children in the DOM tree. 
         */
         //console.log(news_links[0]['children'][0]['children'][1]['children'][1]['children'][0]['data']);
         for(var i = 0; i < news_links.length; i++){
